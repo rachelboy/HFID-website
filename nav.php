33ea0028c125800d88bb472d56ae4256d7d68566
rachelboy <rachel.boy@gmail.com>
2014-10-06 16:14:46 -0400

    OK, now PHP should work

--- nav.php
+++ nav.php
@@ -8,15 +8,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.html">Death By Hypothermia</a>
+      <a class="navbar-brand" href="index.php">Death By Hypothermia</a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li><a href="project-brief.html">Project Brief</a></li>
-        <li><a href="project-brief-new.html">Updated Project Brief</a></li>
-        <li><a href="needs-analysis.html">Needs Analysis</a></li>
-        <li><a href="inspirations.html">Design Inspiration</a></li>
-        <li><a href="storyboard_1.html">Storyboard</a></li>
+        <li><a href="project-brief.php">Project Brief</a></li>
+        <li><a href="project-brief-new.php">Updated Project Brief</a></li>
+        <li><a href="needs-analysis.php">Needs Analysis</a></li>
+        <li><a href="inspirations.php">Design Inspiration</a></li>
+        <li><a href="storyboard_1.php">Storyboard</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div><!--/.container-fluid -->
