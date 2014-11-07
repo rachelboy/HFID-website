bacda63e01359e19ed5c5b725580eff25510913b
rachelboy <rachel.boy@gmail.com>
2014-11-06 23:17:14 -0500

    starting lasgna recipe

--- prototype/javascript/add_recipe.js
+++ prototype/javascript/add_recipe.js
@@ -6,10 +6,10 @@ function add_ingredient()
     var added_names = document.getElementById('names');
     var added_nums = document.getElementById('quantities');
 
-    added_names.innerHTML = added_names.innerHTML + "<p>" + name.value + "</p>"
-    added_nums.innerHTML = added_nums.innerHTML + "<p>" + String(num.value) + " " + units.value + "\n</p>"
+    added_names.innerHTML = added_names.innerHTML + "<p>" + name.value + "</p>";
+    added_nums.innerHTML = added_nums.innerHTML + "<p>" + String(num.value) + " " + units.value + "\n</p>";
 
-    name.value = "next ingredient"
-    num.value = 0
-    units.value = "cups"
+    name.value = "next ingredient";
+    num.value = 0;
+    units.value = "cups";
 }
