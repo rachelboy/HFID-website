21c12ce2f561b1659ae7965bfaa86a0fec0ec6d2
Casey Monahan <caseymariemonahan@gmail.com>
2014-11-06 23:16:23 -0500

    Shopping List check offs
    when clicked the shopping list items turn grey

---
+++ prototype/javascript/crossoff.js
@@ -0,0 +1,14 @@
+function crossoff(listitem)
+{
+    var listitem=document.getElementById(listitem);
+    if (listitem.style.color="black"){
+    	listitem.style.color="silver";
+    } 
+
+	else{
+		listitem.style.color="black"
+	}
+
+
+
+}
---
+++ prototype/javascript/list.js
@@ -0,0 +1,9 @@
+function crossoff(image)
+{
+    var listitem=document.getElementById('listItem_one');
+    if (listitem.style.color="black"){
+    	listitem.style.color="silver"
+    } 
+    else{
+    	listitem.style.color="black"
+    }
