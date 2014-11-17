5ca94cc090668b17e6255cc7f80868fb9500b7a8
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-11-16 19:03:56 -0500

    Created Design Refinement.php

---
+++ design-refinement.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Design Development</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for reports -->
+    <link href="report.css" rel="stylesheet">
+
+    <!-- Custom styles for nav bar -->
+    <link href="nav.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+        <?php include("nav.php"); ?>
+
+
+      <div class="row">
+
+          <div class="report">
+            <h1>Design Development</h1>
+            <p class="date">October 23, 2014</p>
+
+            <h2>What is it?</h2>
+            <p>
+
+            </p>
+
+            
+            <h2> What Changed</h2>
+ 
+
+
+            <h2> Heuristic Evaluation Response</h2>
+
+
+           
+            <h2> Cooking and Grocery Shopping are Complex Processes</h2>
+
+            
+
+            <h2> Everyone is Their Own Expert</h2>
+
+
+            <h2> Autonomy</h2>
+
+            <img src="images/designDevelopment/flowChart.png" alt="Partially Planned Paul" height="500px"/>  
+
+
+          </div>
+
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
