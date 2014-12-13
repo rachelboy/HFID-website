4e465a43f0d780fcd6879c0cc7b76bc9d2c6ca7a
Mateusz Okon <mokon1@babson.edu>
2014-12-13 09:54:09 -0500

    setup

--- prototype/javascript/cookbook.js
+++ prototype/javascript/cookbook.js
@@ -1,7 +1,7 @@
 meals = [];
 
-meals[0] = [prep_for_compare("Stovetop Lasagna"), "images/lasagna.jpg", ["favorites"],4, 3];
-meals[1] = [prep_for_compare("Tomato Soup"), "images/soup.png", ["favorites"],4, 3]
+meals[0] = [prep_for_compare("Stovetop Lasagna"), "images/lasagna.jpg", ["favorites"],4, 3,"lasagna_recipe.html"];
+meals[1] = [prep_for_compare("Tomato Soup"), "images/soup.png", ["favorites"],4, 3,"tomato_soup.html"]
 
 function prep_for_compare(str){
 	return str.toLowerCase().split(" ");
@@ -63,23 +63,40 @@ function add_meal(food){
 	var parent = document.getElementById("meals");
 	var meal = document.createElement("div");
 	meal.class = "meal";
-	var title = document.createElement("h2");
-	title.innerHTML = food[0].join(" ");
+	
+
+	var recipe_link = document.createElement("a");
+	recipe_link.href = food[5];
+	meal.appendChild(recipe_link);
+
+	
+	var meal_description = document.createElement('div');
+	meal_description.class = "meal_description";
+	meal.appendChild(meal_description);
+
 	var image = document.createElement('img');
 	image.src = food[1];
+	meal.appendChild(image);
+	
+
+	var meal_desc_text = document.createElement('div');
+	meal_desc_text.class = "meal_desc_text";
+	meal.appendChild(meal_desc_text);
+	var title = document.createElement("h2");
+	title.innerHTML = food[0].join(" ");
 	var coupons = document.createElement('p');
 	coupons.innerHTML = "Availble Coupons:" + food[4];
 	var servings = document.createElement('p');
 	servings.innerHTML = "Servings:" + food[3];
-	var add_to_list = document.createElement('button');
-	coupons.innerHTML = "Add to List";
-	parent.appendChild(meal);
 	meal.appendChild(title);
 	meal.appendChild(image);
 	meal.appendChild(coupons);
-	meal.appendChild(servings);
-	meal.appendChild(add_to_list);
+	
 
+	var add_to_list = document.createElement('button');
+	coupons.innerHTML = "Add to List";
+	meal.appendChild(add_to_list);
+	
 }
 
 function publish(results){
